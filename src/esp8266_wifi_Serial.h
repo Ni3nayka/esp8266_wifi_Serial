64fbafa73cd07609f7f8e4e04270ccf004964754
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2023-04-22 18:40:29 +0300

    structure

---
+++ src/esp8266_wifi_Serial.h
@@ -0,0 +1,27 @@
+/*
+ * arduino library for solving problems with graphs:
+ * https://github.com/Ni3nayka/esp8266_wifi_Serial
+ *
+ * links:
+ * server:
+ * https://diytech.ru/projects/sozdanie-veb-servera-esp8266-kod-i-skhemy
+ * https://www.embedded-robotics.com/esp8266-wifi/
+ * client:
+ * https://robotzero.one/sending-data-esp8266-to-esp8266/
+ *
+ * author: Egor Bakay <egor_bakay@inbox.ru>
+ * write:  April 2023
+ * modify: April 2023
+ */
+
+#pragma once
+
+#define WIFI_NAME_DEFAULT "uzKoQOg02HYQacJUQGX2"
+#define WIFI_PASSWORD_DEFAULT "3XnoFOYwxYZv3DP9sM0X"
+
+class myWifiSerial {
+    public:
+    private:
+};
+
+myWifiSerial WifiSerial;
